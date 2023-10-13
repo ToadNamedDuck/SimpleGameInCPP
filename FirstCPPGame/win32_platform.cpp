bf25b9f91617f6fccb67a2caf9952a0d4320c00f
Jason Harris <lord.jtharris@gmail.com>
2023-10-13 14:03:01 -0500

    Adjusted some project settings, and have been creating the window class, and registering it.

--- FirstCPPGame/win32_platform.cpp
+++ FirstCPPGame/win32_platform.cpp
@@ -6,11 +6,20 @@
 //Since we are making a graphical interface for the Windows platform, we need to include the windows.h header, and declare a WinMain as our entry point.
 #include <windows.h>
 
-int __clrcall WinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine,
-	int nShowCmd
-) {
+//Create our window callback function.
+LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
 
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) 
+{
+	//We need to create a window class
+	WNDCLASS window_class = {};//Declare window
+	window_class.style = CS_HREDRAW | CS_VREDRAW; //Allows us to draw to the window horizontally and vertically.
+	window_class.lpszClassName = L"Game Window Class";//Give the window a name. L converts the type of the string.
+	window_class.lpfnWndProc = window_callback;//This is the callback function that we use to receive communication from the window. every time something happens, like the windows receives input, this gets called.
+	//Register the window class
+
+	//Create the window!
 }
