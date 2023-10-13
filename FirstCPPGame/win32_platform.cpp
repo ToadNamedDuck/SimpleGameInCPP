f03555d7cd5a8e3906ccfd1a959bb31eb625b2a3
Jason Harris <lord.jtharris@gmail.com>
2023-10-13 14:13:23 -0500

    Window created, need to get it to stay around

--- FirstCPPGame/win32_platform.cpp
+++ FirstCPPGame/win32_platform.cpp
@@ -20,6 +20,12 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 	window_class.lpszClassName = L"Game Window Class";//Give the window a name. L converts the type of the string.
 	window_class.lpfnWndProc = window_callback;//This is the callback function that we use to receive communication from the window. every time something happens, like the windows receives input, this gets called.
 	//Register the window class
-
+		//What we are doing is passing the compiler the address, like a locker number, of our window class so that it may find or modify that information later.
+	RegisterClass(&window_class);//We are registering a reference to window_class. RegisterClass uses the reference to get to our original window_class and register its information.
 	//Create the window!
+
+	//CreateWindow takes the class name of the window, the Title, style options, so overlapped and visible, we let windows decide where to spawn the window, width height
+	//We give it 0 for both WndParent (theres no parent) and for hMenu, pass our WinMain hInstance, and theres no lpParam for us.
+	//That's a LOT
+	CreateWindow(window_class.lpszClassName, L"My First Game in C++", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
 }
