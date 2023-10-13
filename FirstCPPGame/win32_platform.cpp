59fffca5124be370635eaf572050017af1b36461
Jason Harris <lord.jtharris@gmail.com>
2023-10-13 13:38:47 -0500

    init

---
+++ FirstCPPGame/win32_platform.cpp
@@ -0,0 +1,16 @@
+//Code written by ToadNamedDuck, using a tutorial from Dan Zaidan as reference as I follow his tutorial on Youtube.
+
+//This file is the Windows platform-specific code layer - this is PLATFORM DEPENDENT.
+
+//Since we are devloping using the Windows API, we can find resources for future API requests on MSDN.
+//Since we are making a graphical interface for the Windows platform, we need to include the windows.h header, and declare a WinMain as our entry point.
+#include <windows.h>
+
+int __clrcall WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nShowCmd
+) {
+
+}
