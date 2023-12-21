374ce9240bf8d76da6f7b1592846907a02079224
Jason Harris <lord.jtharris@gmail.com>
2023-12-21 16:50:08 -0600

    very minor note for my future reference

--- FirstCPPGame/utils.cpp
+++ FirstCPPGame/utils.cpp
@@ -1,4 +1,13 @@
+typedef char s8;
+typedef unsigned char u8;
+typedef short s16;
+typedef unsigned short u16;
+typedef int s32;
 typedef unsigned int u32;
+typedef long long s64;
+typedef unsigned long long u64;
+//Typedefs are a way for us to create shorthands for typenames that are annoying to type every time.
+//We are at 16:30 in the video. He's doing some more global definitions and whatnot to help make things static, since we are including files a lot.
 
 inline int clamp(int min, int val, int max) {
 	if (val > max) return max;
