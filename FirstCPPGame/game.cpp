2a7b9dbb7f14348953a58dfd42fba524f56eff18
Jason Harris <lord.jtharris@gmail.com>
2023-12-30 12:47:12 -0600

    Added friction and momentum to the movement. It feels more fluid.

--- FirstCPPGame/game.cpp
+++ FirstCPPGame/game.cpp
@@ -2,7 +2,7 @@
 #define pressed(b) (input->buttons[b].is_down && input->buttons[b].changed)//If the button is down and it has changed since the last frame.
 #define released(b) (!input->buttons[b].is_down && input->buttons[b].changed)//If the button is not down and has changed since the last frame.
 
-float player_pos = 0.f;//Player position vertical
+float player_pos, player_derPos;//Player position vertical
 
 
 internal void 
@@ -10,14 +10,20 @@ simulate_game(Input* input, float dt) {
 	clear_screen(0x881253);
 	draw_rect(0, 0, 85, 45, 0x00cb00);
 
-	float speed = 50.f;//Units per second.
-	if (is_down(BUTTON_UP)) player_pos += speed*dt;
-	if (is_down(BUTTON_DOWN)) player_pos -= speed*dt;
+	float player_ddp = 0.f;//Derivitive of the derivitive of the player position. Friction.
+
+	if (is_down(BUTTON_UP)) player_ddp += 2000;
+	if (is_down(BUTTON_DOWN)) player_ddp -= 2000;
+
+	player_ddp -= player_derPos * 10.f;
+
+	player_pos = player_pos + player_derPos * dt + player_ddp * dt * dt * .5f;
+	player_derPos = player_derPos + player_ddp * dt;
 
 
 	draw_rect(0, 0, 1, 1, 0xffffff);//DRAW AFTER INCREMENTATION
 
 	draw_rect(80, 0, 2.5, 12, 0x5522ff);
-	draw_rect(-80, 0, 2.5, 12, 0x5522ff);
+	draw_rect(-80, player_pos, 2.5, 12, 0x5522ff);
 
 }
