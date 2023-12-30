ff2b697dd6eec37b8fbdf15afd4892835b1c4499
Jason Harris <lord.jtharris@gmail.com>
2023-12-30 11:53:31 -0600

    Bugfix and beginning to add stage

--- FirstCPPGame/game.cpp
+++ FirstCPPGame/game.cpp
@@ -2,21 +2,19 @@
 #define pressed(b) (input->buttons[b].is_down && input->buttons[b].changed)//If the button is down and it has changed since the last frame.
 #define released(b) (!input->buttons[b].is_down && input->buttons[b].changed)//If the button is not down and has changed since the last frame.
 
-float player_pos_y = 0.f;//Player position vertical
-float player_pos_x = 0.f;//Player position horizontal
+float player_pos = 0.f;//Player position vertical
 
 
 internal void 
 simulate_game(Input* input, float dt) {
 	clear_screen(0xc285d3);
+	draw_rect(0, 0, 85, 45, 0xaaFFb9);
 
 	float speed = 50.f;//Units per second.
-	if (is_down(BUTTON_UP)) player_pos_y += speed*dt;
-	if (is_down(BUTTON_DOWN)) player_pos_y -= speed*dt;
-	if (is_down(BUTTON_RIGHT)) player_pos_x += speed*dt;
-	if (is_down(BUTTON_LEFT)) player_pos_x -= speed*dt;
+	if (is_down(BUTTON_UP)) player_pos += speed*dt;
+	if (is_down(BUTTON_DOWN)) player_pos -= speed*dt;
 
 
-	draw_rect(player_pos_x, player_pos_y, 2, 2, 0x00ff00);//DRAW AFTER INCREMENTATION
+	draw_rect(0, player_pos, 2, 2, 0x00ff00);//DRAW AFTER INCREMENTATION
 
 }
