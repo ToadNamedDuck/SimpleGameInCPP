d78b0c862aa549912a9acbecabaf14818cca96e6
Jason Harris <lord.jtharris@gmail.com>
2023-12-31 22:06:17 -0600

    collision detection for the walls

--- FirstCPPGame/game.cpp
+++ FirstCPPGame/game.cpp
@@ -3,15 +3,17 @@
 #define released(b) (!input->buttons[b].is_down && input->buttons[b].changed)//If the button is not down and has changed since the last frame.
 
 float player_1_pos, player_1_derPos, player_2_pos, player_2_derPos;//Player position vertical
+float arena_half_size_x = 85.f, arena_half_size_y = 45.f;
+
+float player_half_size_x = 2.5, player_half_size_y = 12;
 
 
 internal void 
 simulate_game(Input* input, float dt) {
 	clear_screen(0x881253);
-	draw_rect(0, 0, 85, 45, 0x00cb00);
+	draw_rect(0, 0, arena_half_size_x, arena_half_size_y, 0x00cb00);//Arena
 
-	float player_1_ddp = 0.f;//Derivitive of the derivitive of the player position. Friction.
-	float player_2_ddp = 0.f;
+	float player_1_ddp = 0.f, player_2_ddp = 0.f;
 
 	if (is_down(BUTTON_W)) player_1_ddp += 2000;
 	if (is_down(BUTTON_S)) player_1_ddp -= 2000;
@@ -24,6 +26,27 @@ simulate_game(Input* input, float dt) {
 	player_1_pos = player_1_pos + player_1_derPos * dt + player_1_ddp * dt * dt * .5f;
 	player_1_derPos = player_1_derPos + player_1_ddp * dt;
 
+	if (player_1_pos + player_half_size_y > arena_half_size_y) {//top
+		//So if these are touching, we move the player back and cut their velocity.
+		player_1_pos = arena_half_size_y - player_half_size_y;
+		player_1_derPos = 0;
+	}
+	if (player_1_pos - player_half_size_y < -arena_half_size_y) {//bottom
+		//So if these are touching, we move the player back and cut their velocity.
+		player_1_pos = -arena_half_size_y + player_half_size_y;
+		player_1_derPos = 0;
+	}
+
+	if (player_2_pos + player_half_size_y > arena_half_size_y) {//top
+		//So if these are touching, we move the player back and cut their velocity.
+		player_2_pos = arena_half_size_y - player_half_size_y;
+		player_2_derPos = 0;
+	}
+	if (player_2_pos - player_half_size_y < -arena_half_size_y) {//bottom
+		//So if these are touching, we move the player back and cut their velocity.
+		player_2_pos = -arena_half_size_y + player_half_size_y;
+		player_2_derPos = 0;
+	}
 
 	player_2_ddp -= player_2_derPos * 10.f;
 
@@ -32,8 +55,8 @@ simulate_game(Input* input, float dt) {
 
 	draw_rect(0, 0, 1, 1, 0xffffff);//DRAW AFTER INCREMENTATION
 
-	draw_rect(-80, player_1_pos, 2.5, 12, 0x5522ff);
-	draw_rect(80, player_2_pos, 2.5, 12, 0x5522ff);
+	draw_rect(-80, player_1_pos, player_half_size_x, player_half_size_y, 0x5522ff);
+	draw_rect(80, player_2_pos, player_half_size_x, player_half_size_y, 0x5522ff);
 
 
 }
