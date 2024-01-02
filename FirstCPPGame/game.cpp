536a7ea221415b28e12f1ca263aee543e92597d4
Jason Harris <lord.jtharris@gmail.com>
2024-01-02 15:08:58 -0600

    Added a scope to ball simulation, helps compression according to OG Doom creator

--- FirstCPPGame/game.cpp
+++ FirstCPPGame/game.cpp
@@ -59,50 +59,53 @@ simulate_game(Input* input, float dt) {
 	simulate_player(&player_1_pos, &player_1_derPos, player_1_ddp, friction, dt);
 	simulate_player(&player_2_pos, &player_2_derPos, player_2_ddp, friction, dt);
 
-	ball_pos_x += ball_derPos_x * dt;//For whatever reason, this is causing the ball to just completely disappear from the screen. It's depressing. 8:30 ish in the video.
-	ball_pos_y += ball_derPos_y * dt;
+	//Simulate Ball
+	{
+		ball_pos_x += ball_derPos_x * dt;
+		ball_pos_y += ball_derPos_y * dt;
+
+
+		//Axis-Aligned Bounding Box collision test. Test for all 4 sides.
+		if (aabb_vs_aabb(ball_pos_x, ball_pos_y, ball_half_size_x, ball_half_size_y, 80, player_2_pos, player_half_size_x, player_half_size_y))
+		{
+			ball_pos_x = 80 - player_half_size_x - ball_half_size_x;
+			ball_derPos_x *= -1;
+			ball_derPos_y = (ball_pos_y - player_2_pos) * 2 + player_2_derPos * .75/*Change velocity by dif in pos of player and ball. Can also add a fraction of player velocity.*/;
+		}
+		else if (aabb_vs_aabb(ball_pos_x, ball_pos_y, ball_half_size_x, ball_half_size_y, -80, player_1_pos, player_half_size_x, player_half_size_y))
+		{
+			ball_pos_x = -80 + player_half_size_x + ball_half_size_x;
+			ball_derPos_x *= -1;
+			ball_derPos_y = (ball_pos_y - player_1_pos) * 2 + player_1_derPos * .75;
+		}
+		//Check for top and bottom of arena collision.
+		if (ball_pos_y + ball_half_size_y > arena_half_size_y)
+		{
+			ball_pos_y = arena_half_size_y - ball_half_size_y;
+			ball_derPos_y *= -1;
+		}
+		else if (ball_pos_y - ball_half_size_y < -arena_half_size_y)
+		{
+			ball_pos_y = -arena_half_size_y + ball_half_size_y;
+			ball_derPos_y *= -1;
+		}
+		if (ball_pos_x + ball_half_size_x > arena_half_size_x)
+		{	//Right and Left
+			//Right side scores point
+			//Reset ball.
+			/*
+			*/
+			ball_pos_x = 0, ball_pos_y = 0, ball_derPos_y = 0;
+		}
+		else if (ball_pos_x - ball_half_size_x < -arena_half_size_x)
+		{
+			ball_pos_x = 0, ball_pos_y = 0, ball_derPos_y = 0;
+		}
+	}
 
 	draw_rect(ball_pos_x, ball_pos_y, ball_half_size_x, ball_half_size_y, 0xffffff);//Ball
-
-
-	//Axis-Aligned Bounding Box collision test. Test for all 4 sides.
-	if(aabb_vs_aabb(ball_pos_x, ball_pos_y, ball_half_size_x, ball_half_size_y, 80, player_2_pos, player_half_size_x, player_half_size_y))
-	{
-		ball_pos_x = 80 - player_half_size_x - ball_half_size_x;
-		ball_derPos_x *= -1;
-		ball_derPos_y = (ball_pos_y - player_2_pos)*2 + player_2_derPos*.75/*Change velocity by dif in pos of player and ball. Can also add a fraction of player velocity.*/;
-	}
-	else if (aabb_vs_aabb(ball_pos_x, ball_pos_y, ball_half_size_x, ball_half_size_y, -80, player_1_pos, player_half_size_x, player_half_size_y))
-	{
-		ball_pos_x = -80 + player_half_size_x + ball_half_size_x;
-		ball_derPos_x *= -1;
-		ball_derPos_y = (ball_pos_y - player_1_pos)*2 + player_1_derPos*.75;
-	}
-
-	//Check for top and bottom of arena collision.
-	if (ball_pos_y + ball_half_size_y > arena_half_size_y) {
-		ball_pos_y = arena_half_size_y - ball_half_size_y;
-		ball_derPos_y *= -1;
-	}
-	else if (ball_pos_y - ball_half_size_y < -arena_half_size_y) {
-		ball_pos_y = -arena_half_size_y + ball_half_size_y;
-		ball_derPos_y *= -1;
-	}
-
-	if (ball_pos_x + ball_half_size_x > arena_half_size_x) {//Right and Left
-		//Right side scores point
-		//Reset ball.
-		/*
-		
-		*/
-		ball_pos_x = 0, ball_pos_y = 0, ball_derPos_y = 0;
-	}
-	else if (ball_pos_x - ball_half_size_x < -arena_half_size_x) {
-		ball_pos_x = 0, ball_pos_y = 0, ball_derPos_y = 0;
-	}
-
-	draw_rect(-80, player_1_pos, player_half_size_x, player_half_size_y, 0x5522ff);
-	draw_rect(80, player_2_pos, player_half_size_x, player_half_size_y, 0x5522ff);
+	draw_rect(-80, player_1_pos, player_half_size_x, player_half_size_y, 0x5522ff);//Right
+	draw_rect(80, player_2_pos, player_half_size_x, player_half_size_y, 0x5522ff);//Left
 
 
 }
