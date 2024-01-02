7b1a45b9e700f2c9bd7afb659d1659e780bf7b8d
Jason Harris <lord.jtharris@gmail.com>
2024-01-02 14:28:02 -0600

    first step in cleanup

--- FirstCPPGame/game.cpp
+++ FirstCPPGame/game.cpp
@@ -7,6 +7,26 @@ float arena_half_size_x = 85.f, arena_half_size_y = 45.f;
 float player_half_size_x = 2.5, player_half_size_y = 12;
 float ball_pos_x, ball_pos_y, ball_derPos_x = 100.f, ball_derPos_y, ball_half_size_x = 1, ball_half_size_y = 1;
 
+//Derivative of position is velocity. DP
+//Derivative of derivative of position is acceleration.
+//Input increases acceleration value, and *friction* can be applied by
+
+internal void
+simulate_player(float *pos, float *vel, float accel, float dt) {
+	accel -= *vel * 10.f;
+	*pos = *pos + *vel * dt + accel * dt * dt * .5f;
+	*vel = *vel + accel * dt;
+
+	if (*pos + player_half_size_y > arena_half_size_y) {//top
+		//So if these are touching, we move the player back and cut their velocity.
+		*pos = arena_half_size_y - player_half_size_y;
+		*vel = 0;
+	}
+	else if (*pos - player_half_size_y < -arena_half_size_y) {//bottom
+		*pos = -arena_half_size_y + player_half_size_y;
+		*vel = 0;
+	}
+}
 
 internal void 
 simulate_game(Input* input, float dt) {
@@ -20,35 +40,8 @@ simulate_game(Input* input, float dt) {
 	if (is_down(BUTTON_UP)) player_2_ddp += 2000;
 	if (is_down(BUTTON_DOWN)) player_2_ddp -= 2000;
 
-
-	player_1_ddp -= player_1_derPos * 10.f;
-
-	player_1_pos = player_1_pos + player_1_derPos * dt + player_1_ddp * dt * dt * .5f;
-	player_1_derPos = player_1_derPos + player_1_ddp * dt;
-
-	player_2_ddp -= player_2_derPos * 10.f;
-
-	player_2_pos = player_2_pos + player_2_derPos * dt + player_2_ddp * dt * dt * .5f;
-	player_2_derPos = player_2_derPos + player_2_ddp * dt;
-
-	if (player_1_pos + player_half_size_y > arena_half_size_y) {//top
-		//So if these are touching, we move the player back and cut their velocity.
-		player_1_pos = arena_half_size_y - player_half_size_y;
-		player_1_derPos = 0;
-	}
-	else if (player_1_pos - player_half_size_y < -arena_half_size_y) {//bottom
-		player_1_pos = -arena_half_size_y + player_half_size_y;
-		player_1_derPos = 0;
-	}
-
-	if (player_2_pos + player_half_size_y > arena_half_size_y) {//top
-		player_2_pos = arena_half_size_y - player_half_size_y;
-		player_2_derPos = 0;
-	}
-	else if (player_2_pos - player_half_size_y < -arena_half_size_y) {//bottom
-		player_2_pos = -arena_half_size_y + player_half_size_y;
-		player_2_derPos = 0;
-	}
+	simulate_player(&player_1_pos, &player_1_derPos, player_1_ddp, dt);
+	simulate_player(&player_2_pos, &player_2_derPos, player_2_ddp, dt);
 
 	ball_pos_x += ball_derPos_x * dt;//For whatever reason, this is causing the ball to just completely disappear from the screen. It's depressing. 8:30 ish in the video.
 	ball_pos_y += ball_derPos_y * dt;
@@ -88,7 +81,7 @@ simulate_game(Input* input, float dt) {
 
 	if (ball_pos_x + ball_half_size_x > arena_half_size_x) {//Right and Left
 		//Right side scores point
-		//Reset ball. 13:23
+		//Reset ball.
 		/*
 		
 		*/
