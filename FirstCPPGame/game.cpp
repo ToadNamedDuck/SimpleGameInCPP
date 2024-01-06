c5af0b30e3eb10f6f974fc5e4bdbad8db7d0e546
Jason Harris <lord.jtharris@gmail.com>
2024-01-05 23:10:15 -0600

    conversion of if statements checking gamemode to case, highlight which button code has selected

--- FirstCPPGame/game.cpp
+++ FirstCPPGame/game.cpp
@@ -47,93 +47,109 @@ aabb_vs_aabb(float p1_x, float p1_y, float p1_hs_x, float p1_hs_y,
 
 //Implement modes to implement menu, and separate the menu from gameplay. Maybe custom pause functionality.
 enum Gamemode {
-	GM_MENU,
+	GM_MENU,//Making a variable from an enum type auto-selects the first member, so I had to change the order of the enum.
 	GM_GAMEPLAY,
 };
 
 Gamemode current_gamemode;
+int hot_button;
 
 internal void 
 simulate_game(Input* input, float dt) {
 	clear_screen(0x881253);
 	draw_rect(0, 0, arena_half_size_x, arena_half_size_y, 0x00cb00);//Arena
 
-	if (current_gamemode == GM_GAMEPLAY) {
 
-		float player_1_ddp = 0.f, player_2_ddp = 0.f;
+	switch (current_gamemode) {
+	case GM_GAMEPLAY: {
+		{
 
-		if (is_down(BUTTON_W)) player_1_ddp += 2000;
-		if (is_down(BUTTON_S)) player_1_ddp -= 2000;
+			float player_1_ddp = 0.f, player_2_ddp = 0.f;
+
+			if (is_down(BUTTON_W)) player_1_ddp += 2000;
+			if (is_down(BUTTON_S)) player_1_ddp -= 2000;
 #if 0
-		if (is_down(BUTTON_UP)) player_2_ddp += 2000;
-		if (is_down(BUTTON_DOWN)) player_2_ddp -= 2000;
+			if (is_down(BUTTON_UP)) player_2_ddp += 2000;
+			if (is_down(BUTTON_DOWN)) player_2_ddp -= 2000;
 #else
-		if ((ball_pos_y - player_2_pos) > 2.f || (ball_pos_y - player_2_pos) < 2.f) {
-			player_2_ddp = ((ball_pos_y - player_2_pos) * 750);
-			if (player_2_ddp > 1600) player_2_ddp = 1600;
-			if (player_2_ddp < -1600) player_2_ddp = -1600;
-		}
-		if (ball_pos_x < 0 && (ball_derPos_y > 40 | ball_derPos_y < -40)) {
-			player_2_ddp *= -.75f;
-		}
+			if ((ball_pos_y - player_2_pos) > 2.f || (ball_pos_y - player_2_pos) < 2.f) {
+				player_2_ddp = ((ball_pos_y - player_2_pos) * 750);
+				if (player_2_ddp > 1600) player_2_ddp = 1600;
+				if (player_2_ddp < -1600) player_2_ddp = -1600;
+			}
+			if (ball_pos_x < 0 && (ball_derPos_y > 40 | ball_derPos_y < -40)) {
+				player_2_ddp *= -.75f;
+			}
 #endif
 
-		simulate_player(&player_1_pos, &player_1_derPos, player_1_ddp, friction, dt);
-		simulate_player(&player_2_pos, &player_2_derPos, player_2_ddp, friction, dt);
+			simulate_player(&player_1_pos, &player_1_derPos, player_1_ddp, friction, dt);
+			simulate_player(&player_2_pos, &player_2_derPos, player_2_ddp, friction, dt);
 
-		//Simulate Ball
-		{
-			ball_pos_x += ball_derPos_x * dt;
-			ball_pos_y += ball_derPos_y * dt;
+			//Simulate Ball
+			{
+				ball_pos_x += ball_derPos_x * dt;
+				ball_pos_y += ball_derPos_y * dt;
 
 
-			//Axis-Aligned Bounding Box collision test. Test for all 4 sides.
-			if (aabb_vs_aabb(ball_pos_x, ball_pos_y, ball_half_size_x, ball_half_size_y, 80, player_2_pos, player_half_size_x, player_half_size_y))
-			{
-				ball_pos_x = 80 - player_half_size_x - ball_half_size_x;
-				ball_derPos_x *= -1;
-				ball_derPos_y = (ball_pos_y - player_2_pos) * 2 + player_2_derPos * .75/*Change velocity by dif in pos of player and ball. Can also add a fraction of player velocity.*/;
+				//Axis-Aligned Bounding Box collision test. Test for all 4 sides.
+				if (aabb_vs_aabb(ball_pos_x, ball_pos_y, ball_half_size_x, ball_half_size_y, 80, player_2_pos, player_half_size_x, player_half_size_y))
+				{
+					ball_pos_x = 80 - player_half_size_x - ball_half_size_x;
+					ball_derPos_x *= -1;
+					ball_derPos_y = (ball_pos_y - player_2_pos) * 2 + player_2_derPos * .75/*Change velocity by dif in pos of player and ball. Can also add a fraction of player velocity.*/;
+				}
+				else if (aabb_vs_aabb(ball_pos_x, ball_pos_y, ball_half_size_x, ball_half_size_y, -80, player_1_pos, player_half_size_x, player_half_size_y))
+				{
+					ball_pos_x = -80 + player_half_size_x + ball_half_size_x;
+					ball_derPos_x *= -1;
+					ball_derPos_y = (ball_pos_y - player_1_pos) * 2 + player_1_derPos * .75;
+				}
+				//Check for top and bottom of arena collision.
+				if (ball_pos_y + ball_half_size_y > arena_half_size_y)
+				{
+					ball_pos_y = arena_half_size_y - ball_half_size_y;
+					ball_derPos_y *= -1;
+				}
+				else if (ball_pos_y - ball_half_size_y < -arena_half_size_y)
+				{
+					ball_pos_y = -arena_half_size_y + ball_half_size_y;
+					ball_derPos_y *= -1;
+				}
+				if (ball_pos_x + ball_half_size_x > arena_half_size_x)
+				{	//Right and Left
+					//Left side scores point
+					//Reset ball.
+					player_2_score += 1;
+					ball_pos_x = 0, ball_pos_y = 0, ball_derPos_y = 0;
+				}
+				else if (ball_pos_x - ball_half_size_x < -arena_half_size_x)
+				{
+					player_1_score += 1;
+					ball_pos_x = 0, ball_pos_y = 0, ball_derPos_y = 0;
+				}
 			}
-			else if (aabb_vs_aabb(ball_pos_x, ball_pos_y, ball_half_size_x, ball_half_size_y, -80, player_1_pos, player_half_size_x, player_half_size_y))
-			{
-				ball_pos_x = -80 + player_half_size_x + ball_half_size_x;
-				ball_derPos_x *= -1;
-				ball_derPos_y = (ball_pos_y - player_1_pos) * 2 + player_1_derPos * .75;
-			}
-			//Check for top and bottom of arena collision.
-			if (ball_pos_y + ball_half_size_y > arena_half_size_y)
-			{
-				ball_pos_y = arena_half_size_y - ball_half_size_y;
-				ball_derPos_y *= -1;
-			}
-			else if (ball_pos_y - ball_half_size_y < -arena_half_size_y)
-			{
-				ball_pos_y = -arena_half_size_y + ball_half_size_y;
-				ball_derPos_y *= -1;
-			}
-			if (ball_pos_x + ball_half_size_x > arena_half_size_x)
-			{	//Right and Left
-				//Left side scores point
-				//Reset ball.
-				player_2_score += 1;
-				ball_pos_x = 0, ball_pos_y = 0, ball_derPos_y = 0;
-			}
-			else if (ball_pos_x - ball_half_size_x < -arena_half_size_x)
-			{
-				player_1_score += 1;
-				ball_pos_x = 0, ball_pos_y = 0, ball_derPos_y = 0;
-			}
+
+			draw_number(player_2_score, -10, 40, 1.f, 0xbbffbb);
+			draw_number(player_1_score, 10, 40, 1.f, 0xbbffbb);
+
+			draw_rect(ball_pos_x, ball_pos_y, ball_half_size_x, ball_half_size_y, 0xffffff);//Ball
+			draw_rect(-80, player_1_pos, player_half_size_x, player_half_size_y, 0x5522ff);//Right
+			draw_rect(80, player_2_pos, player_half_size_x, player_half_size_y, 0x5522ff);//Left
 		}
-
-		draw_number(player_2_score, -10, 40, 1.f, 0xbbffbb);
-		draw_number(player_1_score, 10, 40, 1.f, 0xbbffbb);
-
-		draw_rect(ball_pos_x, ball_pos_y, ball_half_size_x, ball_half_size_y, 0xffffff);//Ball
-		draw_rect(-80, player_1_pos, player_half_size_x, player_half_size_y, 0x5522ff);//Right
-		draw_rect(80, player_2_pos, player_half_size_x, player_half_size_y, 0x5522ff);//Left
-
-	}
-	else {
-		draw_rect(0, 0, 10, 10, 0xff0000);
+	}break;
+	case GM_MENU: {
+		if (hot_button == 0)
+		{
+			draw_rect(20, 0, 10, 10, 0xff0000);
+			draw_rect(-20, 0, 10, 10, 0xffbf00);
+			draw_rect(-20, 0, 8.5, 8.5, 0xff1010);
+		}
+		else
+		{
+			draw_rect(20, 0, 10, 10, 0xffbf00);
+			draw_rect(20, 0, 8.5, 8.5, 0xff1010);
+			draw_rect(-20, 0, 10, 10, 0xff0000);
+		}
+	}break;
 	}
 }
